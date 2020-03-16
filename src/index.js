038018df14c52041d3676213f4e6108a36ef59f8
Kenny Lazer <kennyruzindana10@gmail.com>
2020-03-16 18:27:16 +0200

    App Setup

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+import '@babel/polyfill';
+import express from 'express';
+import fileupload from 'express-fileupload';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import dbConnect from './database';
+// import routes from './routes';
+
+const app = express();
+dbConnect();
+
+app.enable('trust proxy');
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(fileupload({ useTempFiles: true }));
+
+// app.use('/api/v1', routes);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
